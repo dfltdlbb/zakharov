edb552f661fd2c9ec0cc4950ab39528ede22ecb4
dfltdlbb <a23059@ya.ru>
2025-10-17 15:01:13 +0000

    Обновил дату обновления

--- JS/file.js
+++ JS/file.js
@@ -417,7 +417,7 @@ function appendOutput(text){
         break;
 
       case '/date':
-        appendOutput('- Сайт обновлялся последний раз 20 сентября 2025 года');
+        appendOutput('- Сайт обновлялся последний раз 17 октября 2025 года');
         appendOutput('- [Следить на Github](https://github.com/dfltdlbb/zakharov/)');
         break;
 
